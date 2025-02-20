61fed34a151dd1b5d60a46ce87d42a9768faffdb
eelie79 <ellie7901@naver.com>
2025-02-20 20:25:55 +0900

    STEP07: setSquares 변경된  배열 값 입력 방법

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,11 +2,34 @@ import { useState } from "react";
 import { Square } from "./Square";
 
 const Board = () => {
-  const initialSquares = [null, null, null, null, null, null, null, null, null];
+  // const initialSquares = [null, null, null, null, null, null, null, null, null]; // 0~8 각 9개의 게임판 블럭 배열 초기값
+  const initialSquares = Array(9).fill(null);
   const [squares, setSquares] = useState(initialSquares);
 
+  const handleClickEvent = (i) => {
+    console.log(`Square ${i} clicked!`);
+    // squares[i] = "X"; -> 바로 배열에 입력안됨 상태관리 setSquares() 함수 이용하여 값 변경
+
+    // 1. Make a copy of squares state array
+    const newSquares = [...squares];
+
+    // 2. Mutate the copy, setting the i--th element to "X"
+    newSquares[i] = "X";
+
+    // 3. Call the setSquares function with the mutated copy
+    setSquares(newSquares);
+  };
+  console.log(squares);
+
   const renderSquare = (i) => {
-    return <Square value={squares[i]} />;
+    return (
+      <Square
+        value={squares[i]}
+        onClickEvent={() => {
+          handleClickEvent(i);
+        }}
+      />
+    );
   };
 
   return (
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,15 +1,11 @@
 import { useState } from "react";
 
 const Square = (props) => {
-  const [value, setValue] = useState(null);
+  // const [value, setValue] = useState(null);
 
   return (
-    <button
-      className="square"
-      onClick={() => {
-        setValue("X");
-      }}>
-      {value}
+    <button className="square" onClick={props.onClickEvent}>
+      {props.value}
     </button>
   );
 };
